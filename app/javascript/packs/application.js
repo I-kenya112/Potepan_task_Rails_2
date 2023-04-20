54fa4dc80b6308dc39bf77473959c333be13f121
I_Kenya <kenya.imaizumi.prog@gmail.com>
2023-04-20 23:23:26 +0900

    bootstrapを実装 「yarn add bootstrap@5.0.2」 「yarn add @popperjs/core」

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// bootstrapを読み込む
+import "bootstrap";
+import "../stylesheets/application.scss";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()

d93ab3b8d36d409c962f646ab911be59681f23ef
I_Kenya <kenya.imaizumi.prog@gmail.com>
2023-04-29 18:45:54 +0900

    ユーザ編集機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.image = "default-avatar.png"
+    if @user.save
+      log_in @user
+      flash[:success] = "プロフィールを登録しました"
+      redirect_to @user #マイページ
+    else
+      render 'new'
+    end
+  end
+
+  before_action :authenticate_user!
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(update_params)
+      flash[:success] = "プロフィールを更新しました"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
+  has_one_attached :icon
+
+  def icon
+    '/images/profile/default-icon.png'
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true, length: {maximum: 50}
+
+
 end

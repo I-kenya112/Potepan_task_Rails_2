1c86c28c4f4e7e861d8beddeb7baf99e10282213
I_Kenya <kenya.imaizumi.prog@gmail.com>
2023-05-09 23:48:26 +0900

    施設画像を登録シなかった場合のデフォルト画像を設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -30,8 +30,6 @@ class RoomsController < ApplicationController
     @user = current_user
     @room = Room.find(params[:id])
   end
-  
-
 
   def update
     new_params = room_params
@@ -48,6 +46,16 @@ class RoomsController < ApplicationController
   def search
     @q = Room.ransack(params[:q])
     @results = @q.result(distinct: true)
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    if @room.destroy
+      redirect_to rooms_path, notice: "施設を削除しました"
+    else
+      flash[:alert] = "問題が発生しました。"
+      render "index"
+    end
   end
   
   private
--- app/models/room.rb
+++ app/models/room.rb
@@ -8,6 +8,18 @@ class Room < ApplicationRecord
   validates :room_price, presence: true
   validates :room_address, presence: true
 
+  def room_image_url
+    if icon.attached?
+      icon.variant(resize: "400x300").processed
+    else
+      default_room_image_url
+    end
+  end
+
+  def default_room_image_url
+    'default-image-room.png'
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     %w[room_name room_address room_intro]
   end
